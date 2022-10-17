b50fde6c00eca17531e4d17fcfb8fff5c39f24bf
Gustavo <glad.pen1804@fastmail.com>
2022-10-17 08:51:42 -0300

    Adding various development dependencies/tools. Guard, Livereload, Faker, db seeds, etc)

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem "tailwindcss-rails"
 gem "jbuilder"
 
 # Use Redis adapter to run Action Cable in production
-# gem "redis", "~> 4.0"
+gem "redis", "~> 4.0"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
@@ -52,11 +52,15 @@ gem "bootsnap", require: false
 gem "image_processing", "~> 1.2"
 
 group :development, :test do
+  gem "awesome_print"
+  gem "faker"
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
 end
 
 group :development do
+  gem 'guard'
+  gem 'guard-livereload', '~> 2.5', require: false
   gem "better_errors"
   gem "binding_of_caller"
   # Use console on exceptions pages [https://github.com/rails/web-console]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Creating random users with listings, pricing models, rooms, beds etc.
+
+10.times do |i|
+  u = User.create!(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: Faker::Internet.password(min_length: 8))
+
+  rand(1..3).times do |i|
+    l = Listing.create!(user_id: u.id, short_description: Faker::Lorem.sentence(word_count: rand(3..7)), long_description: Faker::Lorem.sentences(number: 3), status: [:draft, :archived].sample, guests: rand(1..10), city: Faker::Address.city, state: Faker::Address.state, postal_code: Faker::Number.number(digits: 5), country: "US", lat: Faker::Address.latitude, lng: Faker::Address.longitude, address_line1: Faker::Address.street_address, address_line2: Faker::Address.secondary_address)
+
+    p = Pricing.create!(listing_id: l.id, value: Faker::Number.number(digits:3), cleaning_fee: Faker::Number.number(digits: 2), status: :active) 
+
+    rand(1..3).times do |i|
+      r = Room.create!(listing_id: l.id, room_type: Room.room_types.keys.sample)
+      
+      rand(1..3).times do |i|
+        Bed.create!(room_id: r.id, bed_size: Bed.bed_sizes.keys.sample)
+      end
+    end
+  end
+end
+
+# Creating development account 1 and 2
+
+10.times do |i|
+  u = User.create!(email: "dev+#{i}@dev.com" , first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "password")
+
+  rand(1..3).times do |i|
+    l = Listing.create!(user_id: u.id, short_description: Faker::Lorem.sentence(word_count: rand(3..7)), long_description: Faker::Lorem.sentences(number: 3), status: [:draft, :archived].sample, guests: rand(1..10), city: Faker::Address.city, state: Faker::Address.state, postal_code: Faker::Number.number(digits: 5), country: "US", lat: Faker::Address.latitude, lng: Faker::Address.longitude, address_line1: Faker::Address.street_address, address_line2: Faker::Address.secondary_address)
+
+    p = Pricing.create!(listing_id: l.id, value: Faker::Number.number(digits:3), cleaning_fee: Faker::Number.number(digits: 2), status: :active) 
+
+    rand(1..3).times do |i|
+      r = Room.create!(listing_id: l.id, room_type: Room.room_types.keys.sample)
+      
+      rand(1..3).times do |i|
+        Bed.create!(room_id: r.id, bed_size: Bed.bed_sizes.keys.sample)
+      end
+    end
+  end
+end
+
+
+
