9d01adfccc9fb437c04bade48fee12c11ed2fab3
Gustavo <glad.pen1804@fastmail.com>
2022-11-21 18:50:54 -0300

    Identation on wishlist_js_controller

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -1,22 +1,28 @@
-import { Controller } from '@hotwired/stimulus';
+import {
+    Controller
+} from '@hotwired/stimulus';
 
 export default class extends Controller {
-  connect() {
-    const csrfToken = document.querySelector("meta[name=csrf-token]").content
-    axios.defaults.headers.common['X-CSRF-Token'] = csrfToken
-  }
-  add_to_wishlist() {
-    let response = axios.post('/wishes', { params: { listing_id: this.data.get("listingId")} })
-		  .then((response) => {
-			  console.log(response.data)
-			  let el = document.getElementById('heart-svg')
-			  if(response.data.wished) {
-                            el.setAttribute('stroke','red')  
-			    el.setAttribute('fill','red')
-			  } else {
-                            el.setAttribute('stroke','currentColor')
-			    el.setAttribute('fill','none')
-			  }
-		  })
-  }
+    connect() {
+        const csrfToken = document.querySelector("meta[name=csrf-token]").content
+        axios.defaults.headers.common['X-CSRF-Token'] = csrfToken
+    }
+    add_to_wishlist() {
+        let response = axios.post('/wishes', {
+                params: {
+                    listing_id: this.data.get("listingId")
+                }
+            })
+            .then((response) => {
+                console.log(response.data)
+                let el = document.getElementById('heart-svg')
+                if (response.data.wished) {
+                    el.setAttribute('stroke', 'red')
+                    el.setAttribute('fill', 'red')
+                } else {
+                    el.setAttribute('stroke', 'currentColor')
+                    el.setAttribute('fill', 'none')
+                }
+            })
+    }
 }
