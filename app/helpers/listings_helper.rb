931ac6b807bbd203fbce9608577d6fa42407539a
Gustavo <glad.pen1804@fastmail.com>
2022-11-23 09:16:33 -0300

    refact: image panels in listing/show

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ListingsHelper
+  def rounding_classes(index)
+    rounding_classes = ["row-span-1 col-span-2  hidden md:block overflow-clip",
+                           "rounded-tr-lg row-span-1 col-span-2  hidden md:block overflow-clip",
+                           "row-span-1 col-span-2  hidden md:block overflow-clip",
+                           "rounded-br-lg row-span-1 col-span-2  hidden md:block overflow-clip"]
+    rounding_classes[index]
+  end
 end
