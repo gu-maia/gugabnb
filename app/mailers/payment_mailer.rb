1a4d5ca04289a9863f2237ccfa9681a5dbcdea10
Gustavo <glad.pen1804@fastmail.com>
2022-12-09 17:24:16 -0300

    Skunk score reduced com 178 to 38

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -2,14 +2,12 @@
 
 class PaymentMailer < ApplicationMailer
   def booking_payment_approved_email
-    @guest = params[:guest]
     @booking = params[:booking]
 
     mail to: @booking.guest.email, subject: 'Your payment was approved!'
   end
 
   def booking_payment_denied_email
-    @guest = params[:guest]
     @booking = params[:booking]
 
     mail to: @booking.guest.email, subject: 'Your payment was denied :('
