054933b84ef22cd978b72960b01fccdaa081eff6
Gustavo <glad.pen1804@fastmail.com>
2022-12-09 20:37:00 -0300

    Rubocop -A

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,12 +1,11 @@
+# frozen_string_literal: true
+
 class WishesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def toggle_wish
     wish = current_user.wishes.find_or_create_by(listing_id: params[:params][:listing_id])
     wish.toggle!(:wished)
     render json: wish
   end
 end
-
-
-
