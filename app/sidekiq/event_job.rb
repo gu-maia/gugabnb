71d87b73e879406f2de63c4dbc7488dd60ef00d3
Gustavo <glad.pen1804@fastmail.com>
2022-12-10 10:14:09 -0300

    event job refact

--- app/sidekiq/event_job.rb
+++ app/sidekiq/event_job.rb
@@ -27,16 +27,23 @@ class EventJob
     checkout_session = stripe_event.data.object
     case stripe_event.type
     when 'checkout.session.completed'
-      booking = Booking.find_by_checkout_session_id(checkout_session.id)
-      booking.update(status: :payment_approved, payment_intent_id: checkout_session.payment_intent)
-      booking.send_payment_approved_notifications
+      update_payment_status(checkout_session)
     when 'charge.refunded'
-      refund = checkout_session.refunds.data[0]
-      booking = Booking.find_by_stripe_refund_id(refund.id)
-      booking.update(status: :refunded)
+      update_refund_status(checkout_session.refunds.data[0])
     when 'charge.failed'
       PaymentDeniedNotification.with(booking: booking).deliver_later(booking.guest.email)
       booking.update(status: :payment_denied)
     end
+  end
+
+  def update_refund_status(refund)
+    booking = Booking.find_by_stripe_refund_id(refund.id)
+    booking.update(status: :refunded)
+  end
+
+  def update_payment_status(checkout_session)
+    booking = Booking.find_by_checkout_session_id(checkout_session.id)
+    booking.update(status: :payment_approved, payment_intent_id: checkout_session.payment_intent)
+    booking.send_payment_approved_notifications
   end
 end
